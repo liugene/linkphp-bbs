7bc806dc1050cd4d7e1af9b30f52ded8fe03ea53
liugene <jun.liu@jrexpo.com>
2017-09-28 17:56:26 +0800

    我的帖子路由支持，登入验证

--- www/resource/static/main/logic/user.js
+++ www/resource/static/main/logic/user.js
@@ -11,32 +11,4 @@ $(function(){
             return false;
         }
     });
-
-    airRouter.map('/user/post',function(){
-        //我的帖子
-        var myUrl = 'index.php/main/user/getPostByUser';
-        getData(myUrl,'',function(err, data){
-            if (err) {
-                layer.msg('获取失败!', {icon: data.code});
-                throw err;
-            }
-            var html = "";
-            $.each(data.data,function(i,item){
-                html += '<div class="right-content-item">'+
-                    '<div class="row">'+
-                    '<div class="row-size">'+
-                    '<span class="label label-warning">123</span>'+
-                    '</div>'+
-                    '<div class="row-title">'+
-                    '<span><a href="javascript:;">'+item.title+'</a></span>'+
-                    '</div>'+
-                    '<div class="row-time">'+
-                    '<span>'+FormatDate(item.post_time)+'</span>'+
-                    '</div>'+
-                    '</div>'+
-                    '</div>';
-            });
-            $('.right-content-box').html(html);
-        });
-    });
 });
--- www/router.js
+++ www/router.js
@@ -88,5 +88,39 @@ $(function(){
         loadCss('/resource/static/main/css/forum.css');
         loadJs('/resource/static/main/logic/items.js');
     });
+    airRouter.map('/user/post',function(){
+        $('#nav').css('display','block');
+        $('#footer').css('display','block');
+        $('.box').css('height','');
+        $('body').css({'height':'','overflow':'','background':'#393D49'});
+        loadJs('/resource/static/main/view/user.js');
+        loadCss('/resource/static/main/css/user.css');
+        loadJs('/resource/static/main/logic/user.js');
+        //我的帖子
+        var myUrl = 'index.php/main/user/getPostByUser';
+        getData(myUrl,'',function(err, data){
+            if (err) {
+                layer.msg('获取失败!', {icon: data.code});
+                throw err;
+            }
+            var html = "";
+            $.each(data.data,function(i,item){
+                html += '<div class="right-content-item">'+
+                    '<div class="row">'+
+                    '<div class="row-size">'+
+                    '<span class="label label-warning">123</span>'+
+                    '</div>'+
+                    '<div class="row-title">'+
+                    '<span><a href="javascript:;">'+item.title+'</a></span>'+
+                    '</div>'+
+                    '<div class="row-time">'+
+                    '<span>'+FormatDate(item.post_time)+'</span>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>';
+            });
+            $('.right-content-box').html(html);
+        });
+    });
     airRouter.init();
 });
